9228467953d0258a1aec42adae82ccf9af8bf4d0
nycpivot <mijames@vmware.com>
2020-06-24 20:57:02 -0400

    Initial commit.

---
+++ src/Tanzu.Dotnet.Core.Web.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tanzu.Dotnet.Core.Web.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
