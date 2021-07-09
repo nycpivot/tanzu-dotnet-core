23b79cd84337415e0dcb828b641cce3c12717591
nycpivot <mijames@vmware.com>
2021-07-09 12:19:40 -0400

    Configured for Iris.

--- src/Tanzu.Dotnet.Core.Web.Mvc/Program.cs
+++ src/Tanzu.Dotnet.Core.Web.Mvc/Program.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System.IO;
 
 namespace Tanzu.Dotnet.Core.Web.Mvc
 {
@@ -20,7 +15,8 @@ namespace Tanzu.Dotnet.Core.Web.Mvc
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder
+                        .UseStartup<Startup>();
                 });
     }
 }
